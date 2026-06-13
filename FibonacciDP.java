4f31ce6e525bf0e26dc5bf9fc43599942a6f92d1
Sakthi Nivethitha  G <sakthinivethitha.g1@gmail.com>
2026-06-13 13:27:16 +0530

    Add files via upload

---
+++ FibonacciDP.java
@@ -0,0 +1,22 @@
+public class FibonacciDP {
+
+	public static void main(String[] args) {
+
+		int n = 10;
+
+		int[] dp = new int[n];
+
+		dp[0] = 0;
+		dp[1] = 1;
+
+		System.out.println("First " + n + " Fibonacci Numbers:");
+
+		System.out.print(dp[0] + " ");
+		System.out.print(dp[1] + " ");
+
+		for (int i = 2; i < n; i++) {
+			dp[i] = dp[i - 1] + dp[i - 2];
+			System.out.print(dp[i] + " ");
+		}
+	}
+}
---
+++ FrogJump.java
@@ -0,0 +1,37 @@
+public class FrogJump {
+
+    public static int frogJump(int[] height) {
+
+        int n = height.length;
+
+        int[] dp = new int[n];
+
+        dp[0] = 0;
+
+        for (int i = 1; i < n; i++) {
+
+            int oneStep =
+                    dp[i - 1]
+                    + Math.abs(height[i] - height[i - 1]);
+
+            int twoStep = Integer.MAX_VALUE;
+
+            if (i > 1) {
+                twoStep =
+                        dp[i - 2]
+                        + Math.abs(height[i] - height[i - 2]);
+            }
+
+            dp[i] = Math.min(oneStep, twoStep);
+        }
+
+        return dp[n - 1];
+    }
+
+    public static void main(String[] args) {
+
+        int[] height = {10, 20, 30, 10};
+
+        System.out.println(frogJump(height));
+    }
+}
